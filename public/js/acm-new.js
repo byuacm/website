fa19aa6a7b24a5b77806541a9baa70a8130375dd
Jeff <jeffburgin@gmail.com>
2015-04-08 12:48:22 -0600

    Worked on routes and how we will use them, they currently work, but I am still playing around to see if I can get the bootstrap active nav to change

---
+++ public/js/acm-new.js
@@ -0,0 +1,35 @@
+angular.module('acm', ['ui.router'])
+.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		$stateProvider.state('home', {
+			url: '/',
+			templateUrl: '/html/home.html',
+			//controller: 'HomeCtrl'
+		})
+		.state('challenges', {
+			url: '/challenges',
+			templateUrl: '/html/challenges.html',
+			//controller: 'ChallengesCtrl'
+		})
+		.state('partner', {
+			url: '/partner',
+			templateUrl: '/html/partner.html',
+			//controller: 'PartnerCtrl'
+		})
+		.state('roster', {
+			url: '/roster',
+			templateUrl: '/html/roster.html',
+			//controller: 'RosterCtrl'
+		});
+		$urlRouterProvider.otherwise('/');
+	}
+])
+.controller('MainCtrl', [
+  '$scope',
+  '$state',
+  function($scope, $state){
+	//console.log($state.$current.name);
+  }
+]);
